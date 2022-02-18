171f8a22e75d5748ad116cc1d60adb0962901ab6
pakse <pakse@DESKTOP-NPVPPCB>
2022-02-18 19:23:30 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/TaeInYun/greenspace.git

--- src/main/java/com/example/demo/vo/MemberVO.java
+++ src/main/java/com/example/demo/vo/MemberVO.java
@@ -4,7 +4,17 @@ import lombok.Data;
 
 @Data
 public class MemberVO {
-	
 	private int no;
-
+	private String id;
+	private String pwd;
+	private String name;
+	private String nickname;
+	private String email;
+	private String phone;
+	private String account_number;
+	private String account_bank;
+	private int point_use;
+	private int point_save;
+	private int levels;
+	private int mem_type;
 }
